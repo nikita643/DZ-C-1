86a1579fb2c49357a914479657dba46c42408ff3
Никита К <fox643@yandex.ru>
2022-12-05 13:35:57 +0300

    первый коммит

---
+++ Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+int [] number = new int [2];
+string [] title = {"первое", "второе"};
+int count = 0;
+while (count < number.Length){
+    Console.Write($"Введите {title[count]} число: ");
+    number[count] = int.Parse(Console.ReadLine());
+    count++;
+}
+int max = number[0];
+count = 0;
+while(count < number.Length)
+{
+    if(max < number[count]){ max = number[count]; }
+    count++;
+}
+Console.WriteLine($"Максимальное число равно: {max}");
